ad6f6fe04c9ce86a051e5e02f02698c13708548a
Atsusan <as2k.sugimoto@gmail.com>
2024-09-30 23:13:13 +0900

    [update] header + FV セクション構築完了

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,30 +1,32 @@
 // top slide loop text
-const swiperText = new Swiper('#js-swiperText', {
-    slidesPerView: "auto",
+const swiperText = new Swiper('.js-swiperText', {
     centeredSlides: true,
+    slidesPerView: "auto",
     spaceBetween: 30,
     loop: true,
-    loopedSlides: 2,
     speed: 5000,
     cssEase: 'linear',
+    pauseOnFocus: false,
+    allowTouchMove: false,
     autoplay: {
-    delay: 0,
-    disableOnInteraction: false,
+        delay: 0,
+        disableOnInteraction: false,
+        reverseDirection: true,
     },
 });
 
 // top slide loop images
-const swiperImages = new Swiper('#js-swiperImages', {
-    slidesPerView: "auto",
+const swiperImages = new Swiper('.js-swiperImages', {
     centeredSlides: true,
+    slidesPerView: "auto",
     spaceBetween: 20,
     loop: true,
     loopedSlides: 2,
     speed: 5000,
     cssEase: 'linear',
+    allowTouchMove: false,
     autoplay: {
         delay: 0,
         disableOnInteraction: false,
-        reverseDirection: true,
     },
 });
