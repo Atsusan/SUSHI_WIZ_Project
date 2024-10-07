b3a5aa5cf0bf19a5faf95180fb5084f710eb60f5
Atsusan <as2k.sugimoto@gmail.com>
2024-10-08 06:38:21 +0900

    [update] smooth-scroll.jsの実装　planページの hoverアクション修正

---
+++ assets/js/smooth-scroll.js
@@ -0,0 +1,19 @@
+// スムーススクロールの機能を追加
+document.addEventListener('DOMContentLoaded', (event) => {
+    // ページ内リンクにイベントリスナーを設定
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault(); // デフォルトの動作をキャンセル
+
+            // スクロール先の要素を取得
+            const targetId = this.getAttribute('href');
+            const targetElement = document.querySelector(targetId);
+
+            // スムーススクロールを実行
+            targetElement.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    });
+});
